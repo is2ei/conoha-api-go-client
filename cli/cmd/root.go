e927310e405eb1ac96e1a6cca9cc9a4df03245f3
Issei Horie <issei.horie@nulab-inc.com>
2018-10-19 00:27:18 +0900

    Added CONTRIBUTING.md

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
@@ -10,6 +13,13 @@ var rootCmd = &cobra.Command{
 	Long: `Yet another ConoHa API client
 			built by is2ei`,
 	Run: func(cmd *cobra.Command, args []string) {
-
+		fmt.Println("Yet another ConoHa API client")
 	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- cli/main.go
+++ cli/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"github.com/is2ei/conoha-api-go-client"
+	"github.com/is2ei/conoha-api-go-client/cli/cmd"
 )
 
 func main() {
 	cmd.Execute()
 }
-
