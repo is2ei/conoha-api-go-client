03e7f585724cfe7a421a1d4c694c5dd172ae86ae
Issei Horie <issei.horie@nulab-inc.com>
2018-10-18 23:34:51 +0900

    Updated CLI interface

--- cli/cli/root.go
+++ cli/cli/root.go
@@ -1,7 +1,11 @@
 package cmd
 
 import (
+	"github.com/is2ei/conoha-api-go-client"
 	"github.com/spf13/cobra"
 )
+
+// global
+var client *
 
 var rootCmd = &cobra.Command{}
---
+++ cli/conoha/version.go
@@ -0,0 +1,13 @@
+package conoha
+
+const (
+	VersionPath = "/"
+)
+
+type Version struct {
+	Status string `json:"status,omitempty"`
+}
+
+func getVersion() {
+
+}
