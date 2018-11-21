ee2f12db35d16c5006fca955091a9837e3e8696e
Issei Horie <issei.horie@nulab-inc.com>
2018-11-21 22:59:48 +0900

    Added "ForceShutdownServer"

---
+++ conoha/compute_force_shutdown.go
@@ -0,0 +1,35 @@
+package conoha
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type computeForceShutdownRequestParam struct {
+	OsStop *osStop `json:"os-stop"`
+}
+
+type osStop struct {
+	ForceShutdown bool `json:"force_shutdown"`
+}
+
+// ForceShutdownServer shutdowns the server forcefully.
+//
+// ConoHa API docs: https://www.conoha.jp/docs/compute-stop_forcibly_vm.html
+func (c *Conoha) ForceShutdownServer(serverId string) (*responseMeta, error) {
+	u := fmt.Sprintf("%s/v2/%s/servers/%s/action", c.ComputeServiceUrl, c.TenantId, serverId)
+
+	o := osStop{
+		ForceShutdown: true,
+	}
+
+	p := computeForceShutdownRequestParam{
+		OsStop: &o,
+	}
+
+	body, _ := json.Marshal(p)
+
+	_, meta, err := c.buildAndExecRequest("POST", u, body)
+
+	return meta, err
+}
---
+++ conoha/compute_force_shutdown_test.go
@@ -0,0 +1,37 @@
+package conoha
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConoha_ForceShutdownServer(t *testing.T) {
+	ts := createMockServer(t, []string{
+		"/compute/server_force_shutdown",
+	})
+	defer ts.Close()
+
+	conoha := NewConoha(
+		ts.URL,
+		ts.URL,
+		ts.URL,
+		ts.URL,
+		ts.URL,
+		ts.URL,
+		ts.URL,
+		ts.URL,
+		ts.URL,
+		ts.URL,
+		"username",
+		"password",
+		"tenant_id",
+		"token",
+	)
+
+	meta, err := conoha.ForceShutdownServer("server_id")
+
+	assert.NoError(t, err)
+
+	assert.IsType(t, new(responseMeta), meta)
+}
