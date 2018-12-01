c82ac98417025000e889e11a1e8bac963b9c96ec
Issei Horie <issei.horie@nulab-inc.com>
2018-12-01 23:57:43 +0900

    Refactor account_order_items.go

--- conoha/account_order_items.go
+++ conoha/account_order_items.go
@@ -1,20 +1,24 @@
 package conoha
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type getAccountOrderItemsResponseParam struct {
-	OrderItems []OrderItem `json:"order_items"`
+	OrderItems []*OrderItem `json:"order_items"`
 }
 
 // OrderItems fetches the orders list.
 //
 // ConoHa API docs: https://www.conoha.jp/docs/account-order-item-list.html
-func (c *Conoha) OrderItems() ([]OrderItem, *ResponseMeta, error) {
+func (c *Conoha) OrderItems() ([]*OrderItem, *ResponseMeta, error) {
+
+	apiEndPoint := fmt.Sprintf("%s/v1/%s/order-items", c.AccountServiceURL, c.TenantID)
+
 	p := getAccountOrderItemsResponseParam{}
 
-	u := c.AccountServiceURL + "/v1/" + c.TenantID + "/order-items"
-
-	contents, meta, err := c.buildAndExecRequest("GET", u, nil)
+	contents, meta, err := c.buildAndExecRequest("GET", apiEndPoint, nil)
 	if err == nil {
 		err = json.Unmarshal(contents, &p)
 	}
--- conoha/account_order_items_test.go
+++ conoha/account_order_items_test.go
@@ -33,7 +33,7 @@ func TestConoha_OrderItems(t *testing.T) {
 
 	assert.NoError(t, err)
 
-	assert.IsType(t, new([]OrderItem), &items)
+	assert.IsType(t, new([]*OrderItem), &items)
 
 	assert.IsType(t, new(ResponseMeta), meta)
 }
