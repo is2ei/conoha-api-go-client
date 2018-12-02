9f7e04146d7907bbcc74456c073f1cdd4731544e
Issei Horie <issei.horie@nulab-inc.com>
2018-12-02 21:40:25 +0900

    Refactor ComputeServersDetail

--- conoha/compute_servers_detail.go
+++ conoha/compute_servers_detail.go
@@ -1,6 +1,9 @@
 package conoha
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type getComputeServersDetailResponseParam struct {
 	Servers []*ComputeServer `json:"servers"`
@@ -10,11 +13,12 @@ type getComputeServersDetailResponseParam struct {
 //
 // ConoHa API docs: https://www.conoha.jp/docs/compute-get_vms_detail_specified.html
 func (c *Conoha) ComputeServersDetail() ([]*ComputeServer, *ResponseMeta, error) {
-	u := c.ComputeServiceURL + "/v2/" + c.TenantID + "/servers/detail"
 
 	p := getComputeServersDetailResponseParam{}
 
-	contents, meta, err := c.buildAndExecRequest("GET", u, nil)
+	apiEndPoint := fmt.Sprintf("%s/v2/%s/servers/detail", c.ComputeServiceURL, c.TenantID)
+
+	contents, meta, err := c.buildAndExecRequest("GET", apiEndPoint, nil)
 	if err == nil {
 		err = json.Unmarshal(contents, &p)
 	}
