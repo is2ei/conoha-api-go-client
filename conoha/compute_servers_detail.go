be9c044e42c9c56b12f392bfcef60c524f2ce3de
Issei Horie <issei.horie@nulab-inc.com>
2018-12-02 21:41:33 +0900

    Refactor ComputeServersDetail

--- conoha/compute_servers_detail.go
+++ conoha/compute_servers_detail.go
@@ -14,9 +14,9 @@ type getComputeServersDetailResponseParam struct {
 // ConoHa API docs: https://www.conoha.jp/docs/compute-get_vms_detail_specified.html
 func (c *Conoha) ComputeServersDetail() ([]*ComputeServer, *ResponseMeta, error) {
 
-	p := getComputeServersDetailResponseParam{}
-
 	apiEndPoint := fmt.Sprintf("%s/v2/%s/servers/detail", c.ComputeServiceURL, c.TenantID)
+
+	p := getComputeServersDetailResponseParam{}
 
 	contents, meta, err := c.buildAndExecRequest("GET", apiEndPoint, nil)
 	if err == nil {
