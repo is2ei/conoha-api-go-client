21a5187896363b50d372e9cbc453c5ad0b0e368c
Issei Horie <issei.horie@nulab-inc.com>
2018-12-02 21:10:33 +0900

    Refactor MailVersion

--- conoha/mail_version.go
+++ conoha/mail_version.go
@@ -1,20 +1,24 @@
 package conoha
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type getMailVersionResponseParam struct {
-	Version Version `json:"version"`
+	Version *Version `json:"version"`
 }
 
 // MailVersion fetches Mail API information.
 //
 // ConoHa API docs: https://www.conoha.jp/docs/paas-mail-get-version-detail.html
-func (c *Conoha) MailVersion() (Version, *ResponseMeta, error) {
+func (c *Conoha) MailVersion() (*Version, *ResponseMeta, error) {
+
+	apiEndPoint := fmt.Sprintf("%s/v1", c.MailServiceURL)
+
 	p := getMailVersionResponseParam{}
 
-	u := c.MailServiceURL + "/v1"
-
-	contents, meta, err := c.buildAndExecRequest("GET", u, nil)
+	contents, meta, err := c.buildAndExecRequest("GET", apiEndPoint, nil)
 	if err == nil {
 		err = json.Unmarshal(contents, &p)
 	}
