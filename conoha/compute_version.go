8c0bdf5792e57f0f742b5437b3227c29d53d08c3
Issei Horie <issei.horie@nulab-inc.com>
2018-12-02 21:31:19 +0900

    Refactor ComputeVersion

--- conoha/compute_version.go
+++ conoha/compute_version.go
@@ -6,18 +6,19 @@ import (
 )
 
 type getComputeVersionResponseParam struct {
-	Version Version `json:"version"`
+	Version *Version `json:"version"`
 }
 
 // ComputeVersion fetches Compute API information.
 //
 // ConoHa API docs: https://www.conoha.jp/docs/compute-get_version_detail.html
-func (c *Conoha) ComputeVersion() (Version, *ResponseMeta, error) {
+func (c *Conoha) ComputeVersion() (*Version, *ResponseMeta, error) {
+
+	apiEndPoint := fmt.Sprintf("%s/v2", c.ComputeServiceURL)
+
 	p := getComputeVersionResponseParam{}
 
-	u := fmt.Sprintf("%s/v2", c.ComputeServiceURL)
-
-	contents, meta, err := c.buildAndExecRequest("GET", u, nil)
+	contents, meta, err := c.buildAndExecRequest("GET", apiEndPoint, nil)
 	if err == nil {
 		err = json.Unmarshal(contents, &p)
 	}
