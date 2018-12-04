f417f5ff2dd7efa3b4e0d842b8799195a40db95e
Issei Horie <issei.horie@nulab-inc.com>
2018-12-05 00:27:32 +0900

    Refactor ComputeFlavorsDetail

--- conoha/compute_flavors_detail.go
+++ conoha/compute_flavors_detail.go
@@ -13,10 +13,12 @@ type getComputeFlavorsDetailResponseParam struct {
 //
 // ConoHa API docs: https://www.conoha.jp/docs/compute-get_flavors_detail.html
 func (c *Conoha) ComputeFlavorsDetail() ([]*ComputeFlavor, *ResponseMeta, error) {
-	u := fmt.Sprintf("%s/v2/%s/flavors/detail", c.ComputeServiceURL, c.TenantID)
 
-	contents, meta, err := c.buildAndExecRequest("GET", u, nil)
+	apiEndPoint := fmt.Sprintf("%s/v2/%s/flavors/detail", c.ComputeServiceURL, c.TenantID)
+
 	p := getComputeFlavorsDetailResponseParam{}
+
+	contents, meta, err := c.buildAndExecRequest("GET", apiEndPoint, nil)
 	if err == nil {
 		err = json.Unmarshal(contents, &p)
 	}
